03c4741e5b11a35bf30f67a8aa11cb5351c82f7e
turrored-ui <turrored@gmail.com>
2026-06-08 14:54:54 -0500

    Update cors.php

--- EntornoDesarrollo/laravel/config/cors.php
+++ EntornoDesarrollo/laravel/config/cors.php
@@ -1,12 +1,14 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'], // Agregamos más rutas
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    // Cambia '*' por tu URL específica para mayor seguridad, 
+    // pero si quieres probar YA, dejar '*' está bien.
+    'allowed_origins' => ['https://panel-frontend-1079064952465.us-central1.run.app'], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true, // <--- ESTO ES LO MÁS IMPORTANTE
 ];
