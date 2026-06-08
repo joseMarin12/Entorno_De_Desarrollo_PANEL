e619d9f6b96fd148ccd47306162e6656c1447f90
turrored-ui <turrored@gmail.com>
2026-06-08 16:10:06 -0500

    Enable supports_credentials in CORS configuration

--- EntornoDesarrollo/laravel/config/cors.php
+++ EntornoDesarrollo/laravel/config/cors.php
@@ -4,6 +4,7 @@ return [
     'paths' => ['api/*', 'login', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://panel-frontend-1079064952465.us-central1.run.app'],
+    'supports_credentials' => true,
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
