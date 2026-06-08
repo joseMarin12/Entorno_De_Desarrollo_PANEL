72467e14476af68da7958f8d4c4d8466110dc3f5
turrored-ui <turrored@gmail.com>
2026-06-08 16:31:48 -0500

    Update cors.php

--- EntornoDesarrollo/laravel/config/cors.php
+++ EntornoDesarrollo/laravel/config/cors.php
@@ -3,6 +3,5 @@ return [
     'paths' => ['api/*', 'login', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://panel-frontend-1079064952465.us-central1.run.app'],
-    'allowed_headers' => ['*'],
-    'supports_credentials' => true, // <-- ESTO ES LO MÁS IMPORTANTE
+    'supports_credentials' => true,
 ];
