07ed52f66799ecf5e552036a4763c2cbb6d1bb9a
turrored-ui <turrored@gmail.com>
2026-06-08 15:01:15 -0500

    Update environment.prod.ts

--- EntornoDesarrollo/angular/src/environments/environment.prod.ts
+++ EntornoDesarrollo/angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://panel-frontend-1079064952465.us-central1.run.app/login',
+  apiUrl: 'https://panel-1079064952465.us-central1.run.app',
 };
