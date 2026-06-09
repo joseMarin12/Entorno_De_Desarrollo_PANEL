c7c0dd984dce96b533bfbeb3e2903f80c3a6a40b
turrored-ui <turrored@gmail.com>
2026-06-09 08:04:39 -0500

    Add logout and register paths to CORS configuration

--- EntornoDesarrollo/laravel/config/cors.php
+++ EntornoDesarrollo/laravel/config/cors.php
@@ -1,7 +1,12 @@
 <?php
+
 return [
-    'paths' => ['api/*', 'login', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'login', 'logout', 'register'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://panel-frontend-1079064952465.us-central1.run.app'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => true,
 ];
