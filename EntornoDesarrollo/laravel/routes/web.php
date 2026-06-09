03c47d61e0c289989897f8c4ebd4b1bb34a58ee4
turrored-ui <turrored@gmail.com>
2026-06-09 13:53:54 -0500

    Update web.php

--- EntornoDesarrollo/laravel/routes/web.php
+++ EntornoDesarrollo/laravel/routes/web.php
@@ -4,14 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\AutenticadorController;
 
-// 🧹 LÍNEAS DE EMERGENCIA: Fuerzan a Cloud Run a borrar la caché vieja en cuanto lee este archivo
 Artisan::call('route:clear');
 Artisan::call('config:clear');
 
-// 🚀 LA RUTA QUE BUSCA ANGULAR: Responde directamente a /login
 Route::post('/login', [AutenticadorController::class, 'login']);
 
-// Tu ruta raíz por defecto (puedes dejarla como esté)
 Route::get('/', function () {
     return view('welcome');
 });
