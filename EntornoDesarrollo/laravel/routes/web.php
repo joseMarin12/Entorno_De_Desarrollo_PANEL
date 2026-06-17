438e2f720c9606da198442c1086db12446f0709b
turrored-ui <turrored@gmail.com>
2026-06-17 13:11:49 -0500

    Update web.php

--- EntornoDesarrollo/laravel/routes/web.php
+++ EntornoDesarrollo/laravel/routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\AutenticadorController;
-
-Artisan::call('route:clear');
-Artisan::call('config:clear');
-
-Route::post('/login', [AutenticadorController::class, 'login']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [AutenticadorController::class, 'login']);
