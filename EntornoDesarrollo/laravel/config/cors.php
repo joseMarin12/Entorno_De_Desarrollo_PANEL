56a1aa19569b6b3f9ab08e27ee6483eea6cb32bc
turrored-ui <turrored@gmail.com>
2026-06-17 13:32:51 -0500

    Update cors.php

--- EntornoDesarrollo/laravel/config/cors.php
+++ EntornoDesarrollo/laravel/config/cors.php
@@ -1,32 +1,12 @@
+<?php
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
-
-    // Solo aplicará a las rutas necesarias (añade aquí más si usas /api/*)
-    'paths' => ['login', 'api/*', 'sanctum/csrf-cookie'],
-
-    // Solo permitimos los métodos que tu app realmente utiliza
-    'allowed_methods' => ['GET', 'POST', 'OPTIONS'],
-
-    // RESTRICCIÓN TOTAL: Solo tu URL de Angular puede hablar con el backend
-    'allowed_origins' => [
-        'https://panel-frontend-1079064952465.us-central1.run.app'
-    ],
-
+    'paths' => ['*', 'api/*', 'login'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-
-    // Solo permitimos los encabezados estándar de peticiones HTTP y autenticación
-    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept', 'X-XSRF-TOKEN'],
-
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
-
-    // Cachea la respuesta del OPTIONS por 24 horas para que el navegador no sature a Laravel
-    'max_age' => 86400,
-
-    // Manténlo en true solo si manejas cookies o sesiones compartidas (Sanctum)
-    'supports_credentials' => true,
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
