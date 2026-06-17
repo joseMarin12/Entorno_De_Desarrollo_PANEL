cf76fb63ba64ee69538ae70aebeb9dd22649a276
turrored-ui <turrored@gmail.com>
2026-06-17 15:25:07 -0500

    Update app.php

--- EntornoDesarrollo/laravel/bootstrap/app.php
+++ EntornoDesarrollo/laravel/bootstrap/app.php
@@ -5,19 +5,17 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        
-        // 🔥 ESTA ES LA LÍNEA CLAVE PARA GOOGLE CLOUD RUN:
-        // Le dice a Laravel que confíe en el balanceador y lea las cabeceras HTTPS reales
-        $middleware->trustProxies(at: '*');
+   ->withMiddleware(function (Middleware $middleware) {
+    
+    // 1. Exceptuar la ruta de login de la verificación CSRF
+    $middleware->validateCsrfTokens(except: [
+        'login', // Si tu ruta es /login
+        'api/login' // Ponlo así también si llegas a usar el prefijo api
+    ]);
 
-    })
+    // 2. Mantener la configuración de los proxies (la que arregló el 405)
+    $middleware->trustProxies(at: '*');
+})
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
