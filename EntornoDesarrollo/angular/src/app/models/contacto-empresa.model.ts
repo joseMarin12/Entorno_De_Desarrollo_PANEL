77221ba087066283459cc283de2d4bd76baa01b6
turrored-ui <turrored@gmail.com>
2026-07-23 09:20:56 -0500

    Update contacto-empresa.model.ts

--- EntornoDesarrollo/angular/src/app/models/contacto-empresa.model.ts
+++ EntornoDesarrollo/angular/src/app/models/contacto-empresa.model.ts
@@ -2,7 +2,11 @@ export interface ContactoEmpresa {
     id: number;
     id_empresa?: number;
     nombre?: string;
+    primer_nombre?: string;
+    segundo_nombre?: string;
     apellido?: string;
+    primer_apellido?: string;   // 🟢 Agregado para resolver el error del HTML (línea 32)
+    segundo_apellido?: string;  // 🟢 Agregado preventivamente
     nombre_completo?: string;
     cargo?: string;
     email?: string;
